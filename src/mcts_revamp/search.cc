5a97f548fd17ab44255f92776884220202cb6cf7
Hans Ekbrand <hans.ekbrand@gmail.com>
2018-12-23 14:29:21 +0100

    sleep 50 milliseconds to test if google cloud then gives us higher speed

--- src/mcts_revamp/search.cc
+++ src/mcts_revamp/search.cc
@@ -278,28 +278,23 @@ void SearchWorker_revamp::RunBlocking() {
       i++;
     }
 
-LOGFILE << "RunNNComputation START";
-// start_comp_time_ = std::chrono::high_resolution_clock::now();
-start_comp_time_ = std::chrono::steady_clock::now();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    LOGFILE << "RunNNComputation START";
+    start_comp_time_ = std::chrono::steady_clock::now();
 
     computation_->ComputeBlocking();
     ic += search_->kMiniBatchSize;
 
-// stop_comp_time_ = std::chrono::high_resolution_clock::now();
-stop_comp_time_ = std::chrono::steady_clock::now();
- auto duration = stop_comp_time_ - start_comp_time_;
- // std::chrono::duration<long, std::micro> duration = stop_comp_time_ - start_comp_time_;
- LOGFILE << "RunNNComputation STOP " << duration.count() << " ";
-
-  int idx_in_computation = search_->kMiniBatchSize;
-
-//  auto duration = stop_comp_time_ - start_comp_time_;
-  int duration_mu = duration.count();
-  if(duration_mu > 0){
-    float better_duration = duration_mu / 1000;
-    float nps = 1000 * idx_in_computation / better_duration;
-    LOGFILE << "nodes in last batch that were evaluated " << idx_in_computation << " nps " << 1000 * nps;
-  }
+    stop_comp_time_ = std::chrono::steady_clock::now();
+    auto duration = stop_comp_time_ - start_comp_time_;
+    LOGFILE << "RunNNComputation STOP " << duration.count() << " ";
+    int idx_in_computation = search_->kMiniBatchSize;
+    int duration_mu = duration.count();
+    if(duration_mu > 0){
+      float better_duration = duration_mu / 1000;
+      float nps = 1000 * idx_in_computation / better_duration;
+      LOGFILE << "nodes in last batch that were evaluated " << idx_in_computation << " nps " << 1000 * nps;
+    }
 
     for (int j = 0; j < search_->kMiniBatchSize; j++) {
       Node_revamp* node = minibatch[j];
